f30695089b3314a9ab9526995810367e51d768ce
allencompsci <Taylor_Hudson@allenisd.org>
2017-09-21 10:21:32 -0500

    afix pause()

--- Period 2/DisplayTextCreateNewProject/DisplayTextCreateNewProject/FName_LName_DisplayText.cpp
+++ Period 2/DisplayTextCreateNewProject/DisplayTextCreateNewProject/FName_LName_DisplayText.cpp
@@ -8,25 +8,27 @@ Create New Project and Display Text to Console
 */
 
 // Libraries
-#include <iostream> // gives access to cin, cout, endl, <<, >>, boolalpha, noboolalpha
-#include <conio.h> // gives access to _kbhit() for pause()
+#include <iostream> //* gives access to cin, cout, endl, <<, >>, boolalpha, noboolalpha
+#include <conio.h> //* gives access to _kbhit() for pause()
 
 // Namespaces
-using namespace std;
+using namespace std; //*
 
 // Functions() 
 void pause() {
 	cout << "Press any key to continue . . .";
 	while (!_kbhit());
-	cout << '\n';
+	int t = _getch();
+	cout << '\n';	
 }
 
 // MAIN
-void main() {
+void main() { // *
 	char text_l = 'l';
 	// Display Text
 	cout << 'H' << 'e' << text_l << text_l << 'o' << " World!" << endl;
 	pause(); // pauses to see the displayed text
+	cout << "example";
 	// return statment closes the console and ends the program
 	return;
 }
