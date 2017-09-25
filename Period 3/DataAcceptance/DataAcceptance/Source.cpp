d6ddeb8e50f956e65bf32f4c58355785c49e537a
allencompsci <Taylor_Hudson@allenisd.org>
2017-09-25 12:32:28 -0500

    Period 3

--- Period 3/DataAcceptance/DataAcceptance/Source.cpp
+++ Period 3/DataAcceptance/DataAcceptance/Source.cpp
@@ -25,12 +25,43 @@ void pause() {
 // MAIN
 void main() {
 	// Variables
+	int age; // How old are you? 
+	char first_letter; // Initial of your First Name?
+	bool in_comp_sci; // Are you in or not?
+	// void doesnt hold value 
+	double GPA; // What is your GPA?
 	
+	int calc_1, calc_2, calc_3, calc_4, calc_5;
+
 	// User Queries 
+	cout << "How old are you? (in years)";
+	cin >> age;
+	cout << "What is the first letter in your first name : (Upper Case Letter)";
+	cin >> first_letter;
+	cout << "Are you in Computer Science (1) or Not (0) : (1/0) ";
+	cin >> in_comp_sci;
+	cout << "What is your GPA? (to the nearest hundredth place) : ";
+	cin >> GPA;
+
+	cout << "Enter a number : ";
+	cin >> calc_1;
+	cout << "Enter a number : ";
+	cin >> calc_2;
+	cout << "Enter a number : ";
+	cin >> calc_3;
 
 	// After questions print out stored data 
-
-
+	cout << "Your age is " << age << endl; // ENDL
+	cout << "Your first initial is " << first_letter << '\n'; // different ways of going to the new line
+	cout << boolalpha << "It is " << in_comp_sci << " that you are in Computer Science. \n";
+	cout << "Your GPA is : " << GPA << "\n";
+	
+	cout << calc_1 << " + " << calc_2 << " = " << calc_1 + calc_2 << endl; // output doing math
+	calc_4 = calc_2 * calc_3;
+	cout << calc_2 << " * " << calc_3 << " = " << calc_4 << '\n';
+	if (calc_4 < calc_2) {
+		cout << "Calc_3 is less than one. " << endl;
+	}
 	// Pause
 	pause();
 }
